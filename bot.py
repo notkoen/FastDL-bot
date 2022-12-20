446f47f0e684023122351656ef3ff9a1d35f1cbb
notkoen <45914779+notkoen@users.noreply.github.com>
2022-12-20 09:51:17 -0800

    Update to 0.3
    -> Added /info to print bot information
    -> Added owner/manager discord ID to .env
    -> Added message to say who to contact should there be any issues
    -> Added bot version variable
    -> All URLs in embed are now shortened and have text
    -> Reworked all embed displays

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
     Name: FastDL query bot by koen
     Author: koen
     Description: A discord bot written in discord.py for players to query map download links from FastDL URL
-    Version: 0.2
+    Version: 0.3
     URL: https://github.com/notkoen
 """
 
@@ -20,6 +20,9 @@ intents.message_content=True
 client=discord.Client(intents=intents)
 tree=app_commands.CommandTree(client)
 
+# Version variable
+VERSION="0.3"
+
 # Load environment variables
 load_dotenv()
 TOKEN=os.getenv('BOT_TOKEN')
@@ -29,20 +32,35 @@ if TOKEN is None:
 FASTDL=os.getenv('FASTDL_URL')
 ALTFASTDL=os.getenv("FASTDL_URL2")
 RESOURCEPACK=os.getenv("RESOURCE_PACK_URL")
+OWNER=os.getenv("BOT_OWNER")
 
 # Bot ready event
 @client.event
 async def on_ready():
     await tree.sync()
-    print("FastDL Query Bot (v0.2) is ready!") # Included version number here
+    print("FastDL Query Bot (v"+VERSION +") is ready!") # Included version number here
+
+# Discord command for retrieving bot information
+@tree.command(name="info", description="Get bot information")
+async def info(interaction: discord.Interaction):
+    embed=discord.Embed(
+        title="FastDL Bot Information",
+        description="",
+        color=0xFFFF00
+    )
+    if OWNER != "":
+        embed.add_field(name="Owner:", value="<@"+OWNER+">", inline=False)
+    embed.add_field(name="Bot Creator:", value="<@265281929295822849>", inline=False)
+    embed.add_field(name="Current Version:", value=VERSION, inline=False)
+    await interaction.response.send_message(embed=embed)
 
 # Discord command for retrieving links directly to both main and alternate FastDL links
-@tree.command(name="fastdl", description="Get FastDL link(s)")
+@tree.command(name="fastdl", description="Get FastDL link")
 async def fastdl(interaction: discord.Interaction):
     if FASTDL == "" and ALTFASTDL == "":
         embed=discord.Embed(
             title="FastDL",
-            description="No FastDL link set! Contact <@265281929295822849> for help!",
+            description="No FastDL link set! Contact <@"+OWNER+"> for help!",
             color=0xFF0000
         )
         await interaction.response.send_message(embed=embed)
@@ -54,11 +72,17 @@ async def fastdl(interaction: discord.Interaction):
                 description="Below are the links to the server's FastDL",
                 color=0x00FF00
             )
-            embed.add_field(name="Main FastDL:", value=FASTDL, inline=False)
-            embed.add_field(name="Backup FastDL:", value=ALTFASTDL, inline=False)
+            embed.add_field(name="​", value="**[Main FastDL]("+FASTDL+")**", inline=False)
+            embed.add_field(name="​", value="**[Backup FastDL]("+ALTFASTDL+")**", inline=False)
             await interaction.response.send_message(embed=embed)
         else:
-            await interaction.response.send_message("Main FastDL:\n" + FASTDL)
+            embed=discord.Embed(
+                title="FastDL",
+                description="Below is the link to the server's FastDL",
+                color=0x00FF00
+            )
+            embed.add_field(name="​", value="**[FastDL]("+FASTDL+")**", inline=False)
+            await interaction.response.send_message(embed=embed)
 
 # Discord command for retrieving resource pack link
 @tree.command(name="resourcepack", description="Get resource pack link")
@@ -66,14 +90,14 @@ async def resourcepack(interaction: discord.Interaction):
     if RESOURCEPACK == "":
         embed=discord.Embed(
             title="Resource Pack",
-            description="No resource pack link was set! Contact <@265281929295822849> for help!",
+            description="No resource pack link was set! Contact <@"+OWNER+"> for help!",
             color=0xFF0000
         )
         await interaction.response.send_message(embed=embed)
     else:
         embed=discord.Embed(
             title="Resource Pack",
-            description=RESOURCEPACK,
+            description="Download [here]("+RESOURCEPACK+")",
             color=0x00FF00
         )
         await interaction.response.send_message(embed=embed)
@@ -86,7 +110,7 @@ async def downloadmap(interaction: discord.Interaction, map_name: str, big_map: 
     if FASTDL == "" and ALTFASTDL == "":
         embed=discord.Embed(
             title="FastDL",
-            description="No FastDL link set! Contact <@265281929295822849> for help!",
+            description="No FastDL link set! Contact <@"+OWNER+"> for help!",
             color=0xFF0000
         )
         await interaction.response.send_message(embed=embed)
@@ -96,41 +120,41 @@ async def downloadmap(interaction: discord.Interaction, map_name: str, big_map: 
             if ALTFASTDL != "":
                 embed=discord.Embed(
                     title="Map Download Link",
-                    description="`" + map_name + "`",
+                    description="`"+map_name+"`",
                     color=0x00FF00
                 )
-                embed.add_field(name="Download Link:", value=FASTDL + map_name + ".bsp", inline=False)
-                embed.add_field(name="Backup Download Link:", value=ALTFASTDL + map_name + ".bsp", inline=False)
-                embed.set_footer(text="If the download links don't work, try setting 'big_map' to false!")
+                embed.add_field(name="​", value="**[Download here]("+FASTDL+map_name+".bsp)**", inline=False)
+                embed.add_field(name="Download not working?", value="**Try this backup [download link]("+ALTFASTDL+map_name+".bsp)**", inline=False)
+                embed.set_footer(text="If the links don't work, try setting 'big_map' to false")
                 await interaction.response.send_message(embed=embed)
             else:
                 embed=discord.Embed(
                     title="Map Download Link",
-                    description="`" + map_name + "`",
+                    description="`"+map_name+"`",
                     color=0x00FF00
                 )
-                embed.add_field(name="Download Link:", value=FASTDL + map_name + ".bsp", inline=False)
-                embed.set_footer(text="If the download link doesn't work, try setting 'big_map' to false!")
+                embed.add_field(name="​", value="**[Download here]("+FASTDL+map_name+".bsp)**", inline=False)
+                embed.set_footer(text="If the link doesn't work, try setting 'big_map' to false")
                 await interaction.response.send_message(embed=embed)
         case False:
             if ALTFASTDL != "":
                 embed=discord.Embed(
                     title="Map Download Link",
-                    description="`" + map_name + "`",
+                    description="`"+map_name+"`",
                     color=0x00FF00
                 )
-                embed.add_field(name="Download Link:", value=FASTDL + map_name + ".bsp.bz2", inline=False)
-                embed.add_field(name="Backup Download Link:", value=ALTFASTDL + map_name + ".bsp.bz2", inline=False)
-                embed.set_footer(text="If the download links don't work, try setting 'big_map' to false!")
+                embed.add_field(name="​", value="**[Download here]("+FASTDL+map_name+".bsp.bz2)**", inline=False)
+                embed.add_field(name="Download not working?", value="**Try this backup [download link]("+ALTFASTDL+map_name+".bsp.bz2)**", inline=False)
+                embed.set_footer(text="If the links don't work, try setting 'big_map' to false")
                 await interaction.response.send_message(embed=embed)
             else:
                 embed=discord.Embed(
                     title="Map Download Link",
-                    description="`" + map_name + "`",
+                    description="`"+map_name+"`",
                     color=0x00FF00
                 )
-                embed.add_field(name="Download Link:", value=FASTDL + map_name + ".bsp.bz2", inline=False)
-                embed.set_footer(text="If the download link doesn't work, try setting 'big_map' to false!")
+                embed.add_field(name="​", value="**[Download here]("+FASTDL+map_name+".bsp.bz2)**", inline=False)
+                embed.set_footer(text="If the link doesn't work, try setting 'big_map' to false")
                 await interaction.response.send_message(embed=embed)
 
 # Start up the bot
